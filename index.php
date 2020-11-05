249d04bc525afa8c809475ba7b2354ca76630e20
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-05 10:20:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -52,18 +52,18 @@ while($row=mysqli_fetch_assoc($result)){
   echo("</tr>"); } 
 echo('</table>'); 
   
-        $sql ="select imie,zarobki from pracownicy where id_org=dzial group by dzial having avg(zarobki)<40"; 
+     $sql ="select imie,zarobki from pracownicy where imie not like '%a' having zarobki<40"; 
 echo("<h3>zadanie3</h3>"); 
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
+echo ("<tr><th>imie</th><th>zarobki</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['avg(zarobki)']."</td><td>".$row['nazwa_dzial']."</td>");     
+  echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
-?>
+  ?>
   </body>
 </html>
