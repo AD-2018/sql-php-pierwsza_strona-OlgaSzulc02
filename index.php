3820513c4bbfd44bf0844a10da720242943e05c1
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-12 08:30:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a>
   
     <div class="nav">
-      <a href="pracownicy_organizacja.php">Pracownicy i organizacja</a>
+      <a href="pracownicy_organizacja.php">Pracownicy i organizacja</a><br>
       <a href="funkcje_agregujace.php">Funkcje Agregujące</a>
       </div>
 
