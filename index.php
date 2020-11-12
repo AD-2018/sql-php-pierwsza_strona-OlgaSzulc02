8b18f11af44b009edab70358c00987552749cb26
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-12 08:36:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $dbname = "sql7374844";
 $conn= new mysqli($servername,$username,$password,$dbname);  
 $sql ="select * from pracownicy"; 
 echo("<h3>tabelka</h3>"); 
+    echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
@@ -33,6 +34,7 @@ echo('</table>');
   
     $sql ="select * from organizacja"; 
 echo("<h3>tabelka organizacja</h3>"); 
+    echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>id_org</th><th>nazwa_dzial</th></tr>"); 
@@ -46,6 +48,7 @@ echo('</table>');
     
     $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial having avg(zarobki)<35"; 
 echo("<h3>zadanie1</h3>"); 
+    echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
@@ -59,6 +62,7 @@ echo('</table>');
   
       $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial group by dzial having avg(zarobki)<40"; 
 echo("<h3>zadanie2</h3>"); 
+    echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
@@ -72,6 +76,7 @@ echo('</table>');
   
         $sql ="select imie,zarobki from pracownicy where imie not like '%a' having zarobki<40"; 
 echo("<h3>zadanie3</h3>"); 
+    echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>imie</th><th>zarobki</th></tr>"); 
@@ -85,6 +90,7 @@ echo('</table>');
   
           $sql ="select nazwa_dzial from pracownicy,organizacja where id_org=dzial and (dzial=2 or dzial=3) group by dzial"; 
 echo("<h3>zadanie4</h3>"); 
+    echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>nazwa_dzial</th></tr>"); 
