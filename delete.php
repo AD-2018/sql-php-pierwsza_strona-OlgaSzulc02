70988158b7468b7c5d754c992f3895c7ec1a0323
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-03 07:58:11 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,18 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
 echo("Delete<br>");
 echo $_POST['id_pracownicy'];
 
 require_once("connect.php");
 
-$sql = "DELETE FROM pracownicy WHERE id=".$_POST['id_pracownicy'];
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
 
 echo $sql;
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+    header('Location: https://olga-szulc.herokuapp.com/danedobazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 ?>
+</body>
+</html>
