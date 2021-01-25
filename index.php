b031157deafc556171ba85f32bb6a2b583632a3e
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2021-01-25 09:01:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
   
     <div class="nav">
       <a href="pracownicy/pracownicy_organizacja.php">Pracownicy i organizacja</a><br>
-      <a href="funkcje_agregujace.php">Funkcje Agregujące</a><br>
-      <a href="data_i_czas.php">Data i Czas</a><br>
+      <a href="pracownicy/funkcje_agregujace.php">Funkcje Agregujące</a><br>
+      <a href="pracownicy/data_i_czas.php">Data i Czas</a><br>
         <a href="formularz.html">Formularz</a><br>
-        <a href="biblioteka.php">Biblioteka</a><br>
-        <a href="danedobazy.php">Dodawanie i usuwanie</a><br>
+        <a href="biblioteka/biblioteka.php">Biblioteka</a><br>
+        <a href="pracownicy/danedobazy.php">Dodawanie i usuwanie</a><br>
       </div>
 
 <?php 
