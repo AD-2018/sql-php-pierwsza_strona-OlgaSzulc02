ef253814fa3a8bd473502885b21a826d653804c7
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2021-01-28 14:38:22 +0100

    Update biblioteka.php

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -9,22 +9,22 @@
       <a href="index.php">Strona Główna</a>
     </div>
 <?php 
-		require_once("connect.php");
-    echo ('<h2>Biblioteka<h2>');
-	$sql ="select autor, tytul from biblAutor,biblTytul, biblTytul_biblAutor where biblTytul_id=biblAutor_id group by ";
+	require_once("connect.php");
+$sql = "select (biblTytul_biblAutor.id) as ID_TAB, autor, tytul, biblWypoz from biblTytul_biblAutor,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
 $result = mysqli_query($conn, $sql);
 if ( $result) {
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 echo('<select name="ksiazka">');
-	while($row = mysqli_fetch_assoc($result)) {
+    while($row = mysqli_fetch_assoc($result)) {
             echo '<option value="'.$row['id'].'">';
-	    echo($row['autor'].', '.$row['tytul']);
- 	    echo "</option>";
-	};
+        echo($row['autor'].', '.$row['tytul']);
+         echo "</option>";
+    };
 echo('</select>');
 echo ('<br>');
+	
 	
 require_once("connect.php");
   echo("<h3>Tytuły</h3>"); 
