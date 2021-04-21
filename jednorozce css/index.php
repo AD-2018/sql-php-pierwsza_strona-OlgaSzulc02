6d2f186795f6b4995f170d8ba42d118b111a6b1b
OlgaSzulc02 <olga.szulc24@gmail.com>
2021-04-21 20:00:48 +0200

    s

--- jednorozce css/index.php
+++ jednorozce css/index.php
@@ -10,18 +10,6 @@
      <h1>Olga Szulc</h1>
         <?php include"menu.php" ?>
         <img src="zadania.png" alt="zadania css" class="zdjecie"/>
-        <li class="gridLinkLi"><a class="gridLink" href="str1/strona.html">Strona 1</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str2/strona.php">Strona 2</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str3/strona.php">Strona 3</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str4/strona.php">Strona 4</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str5/strona.php">Strona 5</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str6/strona.html">Strona 6</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str7/strona.html">Strona 7</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str8/strona.html">Strona 8</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str9/strona.html">Strona 9</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str10/strona.html">Strona 10</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str11/strona.html">Strona 11</a></li>
-        <li class="gridLinkLi"><a class="gridLink" href="str12/strona.html">Strona 12</a></li>
 
     </body>
 </html>
