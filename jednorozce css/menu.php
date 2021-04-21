8b9e0209254b749d980d98ee5b2d27a40bfe034d
OlgaSzulc02 <olga.szulc24@gmail.com>
2021-04-21 20:03:20 +0200

    dd

--- jednorozce css/menu.php
+++ jednorozce css/menu.php
@@ -1,20 +1,20 @@
 <nav> 
     <div class="nav">
     <strong>
-        <li><a class="git" href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a></li>
-        <li><a href="../index.php">Strona główna</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str1/strona.html">Strona 1</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str2/strona.php">Strona 2</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str3/strona.php">Strona 3</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str4/strona.php">Strona 4</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str5/strona.php">Strona 5</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str6/strona.html">Strona 6</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str7/strona.html">Strona 7</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str8/strona.html">Strona 8</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str9/strona.html">Strona 9</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str10/strona.html">Strona 10</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str11/strona.html">Strona 11</a></li>
-        <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str12/strona.html">Strona 12</a></li>
+        <a class="git" href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a></li>
+        <a href="../index.php">Strona główna</a></li>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str1/strona.html">Strona 1</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str2/strona.php">Strona 2</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str3/strona.php">Strona 3</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str4/strona.php">Strona 4</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str5/strona.php">Strona 5</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str6/strona.html">Strona 6</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str7/strona.html">Strona 7</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str8/strona.html">Strona 8</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str9/strona.html">Strona 9</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str10/strona.html">Strona 10</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str11/strona.html">Strona 11</a></br>
+        <a href="https://bazy-danych.herokuapp.com/jednorozce%20css/str12/strona.html">Strona 12</a></br>
     </strong>
     </div>
         </br></br></br>
