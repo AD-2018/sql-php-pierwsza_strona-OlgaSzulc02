a4ae42ff2e861172e07a45f1b63a91956a0bdc2f
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2021-04-27 11:45:12 +0200

    na praktykach

---
+++ menu.php
@@ -0,0 +1,14 @@
+  
+<ol>
+  <li>Menu
+    <ul>
+        <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a></li>
+        <li><a href="pracownicy/pracownicy_organizacja.php">Pracownicy i organizacja</a></li>
+        <li><a href="pracownicy/funkcje_agregujace.php">Funkcje Agregujące</a></li>
+        <li><a href="pracownicy/data_i_czas.php">Data i Czas</a></li>
+        <li><a href="inne/formularz.html">Formularz</a></li>
+        <li><a href="biblioteka/biblioteka.php">Biblioteka</a></li>
+        <li><a href="pracownicy/danedobazy.php">Dodawanie i usuwanie</a></li>
+        <li><a href="jednorozce css/index.php">Jednorozce str głowna</a></li>
+    </ul>
+</ol>
