d45ad7ce991d44a907eba3dea31a677a26c6bf2e
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2021-04-28 13:48:06 +0200

    Create menu2.php

---
+++ jednorozce css/menu2.php
@@ -0,0 +1,8 @@
+<ol>
+    <li>Menu
+    <ul>
+      <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a></li>
+      <li><a href="https://bazy-danych.herokuapp.com/index.php">Strona główna</a></li>
+      <li><a href="https://bazy-danych.herokuapp.com/jednorozce%20css/index.php">Cofnij</a></li>
+    </ul>
+ </ol>   
